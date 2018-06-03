c2e05a755e7789d94895477280692597f0d5fd9d
Erwangf <erwangf@gmail.com>
2018-06-04 01:09:02 +0200

    up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+let Page = require("../models/page");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    Page.find({})
+        .sort({'createdAt': -1})
+        .limit(3)
+        .exec(function (err, pages) {
+            var lastPage = pages;
+            if(pages == null){
+                lastPage = [];
+            }
+            res.render('index', {lastPages : lastPage});
+        });
 });
 
 module.exports = router;
